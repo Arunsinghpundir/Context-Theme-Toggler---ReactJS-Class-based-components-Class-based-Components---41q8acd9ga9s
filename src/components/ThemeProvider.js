ed25f4622eaf1ff48084817ca0da38a6e88f684e
Arun Singh Pundir <101244758+Arunsinghpundir@users.noreply.github.com>
2022-12-08 23:05:07 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -3,7 +3,20 @@ import React from 'react';
 const ThemeContext = React.createContext()
 const ThemeProvider = (props) =>{
 
+    return (import React, { useState } from 'react';
+
+const ThemeContext = React.createContext()
+const ThemeProvider = (props) =>{
+     const [theme, setTheme] = useState('light')
     return (
+        <React.Fragment>
+            <ThemeContext.Provider value={{theme, setTheme}}>
+                {props.children}
+            </ThemeContext.Provider>
+        </React.Fragment>
+    )
+}
+export {ThemeProvider,ThemeContext}
         <React.Fragment>
 
         </React.Fragment>
